d19fe85af93a383e6a7a4b581e74f5489dfacbda
Jeeva Periyasamy <mohanjeeva@users.noreply.github.com>
2026-09-16 19:16:24 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,11 @@
+var word = "Jeeva"
+var num = 10
+var bool = true
+var undef //undefined
+var nul = null
+
+console.log(word)
+console.log(num)
+console.log(bool)
+console.log(typeof undef)
+console.log(typeof nul)
